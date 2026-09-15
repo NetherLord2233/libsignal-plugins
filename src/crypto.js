c21ccc71d750fd625e60fa964e0266fd894a45c4
亗𝙽𝚎𝚝𝚑𝚎𝚛𝙻𝚘𝚛𝚍亗 <cronicasautomovilisticas@gmail.com>
2026-09-15 17:14:25 -0300

    Actualizar crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -1,19 +1,27 @@
 import libsignal from '../index.js';
 
+export const encrypt = libsignal.encrypt || libsignal.encryptData;
+export const decrypt = libsignal.decrypt || libsignal.decryptData;
 export const encryptData = libsignal.encryptData;
 export const decryptData = libsignal.decryptData;
-export const calculateMAC = libsignal.calculateMAC;
-export const verifyMAC = libsignal.verifyMAC;
+export const calculateMac = libsignal.calculateMac;
+export const calculateMAC = libsignal.calculateMac;
+export const verifyMac = libsignal.verifyMac;
+export const verifyMAC = libsignal.verifyMac;
 export const deriveSecrets = libsignal.deriveSecrets;
 export const hash = libsignal.hash;
-export const encryptWhisperMessage = libsignal.encryptWhisperMe>
+export const encryptWhisperMessage = libsignal.encryptWhisperMessage;
 export const fillMessageKeys = libsignal.fillMessageKeys;
 
 export const crypto = {
+  encrypt,
+  decrypt,
   encryptData: libsignal.encryptData,
   decryptData: libsignal.decryptData,
-  calculateMAC: libsignal.calculateMAC,
-  verifyMAC: libsignal.verifyMAC,
+  calculateMac: libsignal.calculateMac,
+  calculateMAC: libsignal.calculateMac,
+  verifyMac: libsignal.verifyMac,
+  verifyMAC: libsignal.verifyMac,
   deriveSecrets: libsignal.deriveSecrets,
   hash: libsignal.hash,
   encryptWhisperMessage: libsignal.encryptWhisperMessage,
