2deb8a25fc789cab830e029153d245f0ecd17615
亗𝙽𝚎𝚝𝚑𝚎𝚛𝙻𝚘𝚛𝚍亗 <cronicasautomovilisticas@gmail.com>
2026-09-15 17:11:04 -0300

    Actualizar crypto.js

--- src/crypto.js
+++ src/crypto.js
@@ -2,18 +2,18 @@ import libsignal from '../index.js';
 
 export const encryptData = libsignal.encryptData;
 export const decryptData = libsignal.decryptData;
-export const calculateMac = libsignal.calculateMac;
-export const verifyMac = libsignal.verifyMac;
+export const calculateMAC = libsignal.calculateMAC;
+export const verifyMAC = libsignal.verifyMAC;
 export const deriveSecrets = libsignal.deriveSecrets;
 export const hash = libsignal.hash;
-export const encryptWhisperMessage = libsignal.encryptWhisperMessage;
+export const encryptWhisperMessage = libsignal.encryptWhisperMe>
 export const fillMessageKeys = libsignal.fillMessageKeys;
 
 export const crypto = {
   encryptData: libsignal.encryptData,
   decryptData: libsignal.decryptData,
-  calculateMac: libsignal.calculateMac,
-  verifyMac: libsignal.verifyMac,
+  calculateMAC: libsignal.calculateMAC,
+  verifyMAC: libsignal.verifyMAC,
   deriveSecrets: libsignal.deriveSecrets,
   hash: libsignal.hash,
   encryptWhisperMessage: libsignal.encryptWhisperMessage,
