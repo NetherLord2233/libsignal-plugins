3316fa9095a7c996c90b5c74d84a96280a30d1b3
亗𝙽𝚎𝚝𝚑𝚎𝚛𝙻𝚘𝚛𝚍亗 <cronicasautomovilisticas@gmail.com>
2026-09-15 17:12:21 -0300

    Actualizar curve.js

--- src/curve.js
+++ src/curve.js
@@ -3,6 +3,7 @@ import libsignal from '../index.js';
 export const generateKeyPair = libsignal.generateKeyPair;
 export const calculateAgreement = libsignal.calculateAgreement;
 export const verifySignature = libsignal.verifySignature;
+export const calculateSignature = libsignal.calculateSignature || libsignal.curve25519Sign;
 export const calculateMac = libsignal.calculateMac;
 export const createKeyPair = libsignal.createKeyPair;
 export const curve25519Sign = libsignal.curve25519Sign;
@@ -11,6 +12,7 @@ export const Curve = {
   generateKeyPair: libsignal.generateKeyPair,
   calculateAgreement: libsignal.calculateAgreement,
   verifySignature: libsignal.verifySignature,
+  calculateSignature: libsignal.calculateSignature || libsignal.curve25519Sign,
   calculateMac: libsignal.calculateMac,
   createKeyPair: libsignal.createKeyPair,
   curve25519Sign: libsignal.curve25519Sign,
