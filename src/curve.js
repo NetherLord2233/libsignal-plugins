504e18b32f9e66cdc7a1a63254db3e9aa68e578f
亗𝙽𝚎𝚝𝚑𝚎𝚛𝙻𝚘𝚛𝚍亗 <cronicasautomovilisticas@gmail.com>
2026-09-15 17:05:48 -0300

    Crear curve.js

---
+++ src/curve.js
@@ -0,0 +1,20 @@
+import libsignal from '../index.js';
+
+export const generateKeyPair = libsignal.generateKeyPair;
+export const calculateAgreement = libsignal.calculateAgreement;
+export const verifySignature = libsignal.verifySignature;
+export const calculateMac = libsignal.calculateMac;
+export const createKeyPair = libsignal.createKeyPair;
+export const curve25519Sign = libsignal.curve25519Sign;
+
+export const Curve = {
+  generateKeyPair: libsignal.generateKeyPair,
+  calculateAgreement: libsignal.calculateAgreement,
+  verifySignature: libsignal.verifySignature,
+  calculateMac: libsignal.calculateMac,
+  createKeyPair: libsignal.createKeyPair,
+  curve25519Sign: libsignal.curve25519Sign,
+  ...libsignal
+};
+
+export default Curve;
